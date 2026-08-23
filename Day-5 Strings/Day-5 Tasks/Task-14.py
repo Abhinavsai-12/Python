8d95bbf7fa3657317ff5b84cb596011dfa5174db
Abhinavsai-12 <abhinavsai1252@gmail.com>
2026-08-23 15:07:11 +0530

    Day-4 Lists

--- Day-5 Strings/Day-5 Tasks/Task-14.py
+++
@@ -1,9 +0,0 @@
-# Task 14: Using replace() method
-
-paragraph = input("Enter a paragraph: ")
-
-paragraph = paragraph.replace("is", "are")
-paragraph = paragraph.replace("the", "that")
-
-print("Modified paragraph:")
-print(paragraph)
